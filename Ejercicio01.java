6ba294071be01ac4503dc11279da031840163f17
luc2210 <lucianuartrabajos@gmail.com>
2024-11-28 13:47:40 +0100

    Add files via upload

---
+++ Ejercicio01.java
@@ -0,0 +1,14 @@
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		String cadena0 = " "; 
+		String cadena1 = "";
+		System.out.println("la cadena 0 es: " + cadena0 + "."); 
+		System.out.println("la cadena 1 es: " + cadena1 + ".");
+		
+		System.out.println(cadena0.isEmpty());
+		System.out.println(cadena1.isEmpty());
+	}
+
+}
